3957b90bda196ef4cf88494d869fd0c74a75ae9b
uiwhwjbjk <154791921+uiwhwjbjk@users.noreply.github.com>
2024-01-10 19:03:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,17 +7,17 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94773283124', 'GURU', true],
+  ['94773283124', 'Ash', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.mods = ['94773283124','94773283124'] 
+global.prems = ['94773283124', '94773283124', '919398758484']
+global.allowed = ['94773283124', '94773283124', '19152999993']
+global.keysZens = ['94773283124', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
